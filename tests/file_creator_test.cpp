36e5bce9babd4225e81fe4acea091acf45c8a0cc
SystemProgrammerWizzard <tatandat110105@gmail.com>
2025-04-10 11:17:13 -0400

    [Test]: Implement the logic for file_creator_test.cpp
    Signed-off-by: SystemProgrammerWizzard <tatandat110105@gmail.com>

--- tests/file_creator_test.cpp
+++ tests/file_creator_test.cpp
@@ -27,10 +27,27 @@ protected:
     }
 };
 
-TEST_F(file_creator_test, write_data_to_non_existing_file)
+TEST_F(file_creator_test, write_and_read_data_from_file)
 {
-    std::cout<<" This is the test!"<<std::endl;
-    EXPECT_TRUE(true);
-}
+    std::string test_dir = "test_output";
+    std::filesystem::create_directory(test_dir);
 
+    std::string file_path = test_dir;
+    std::string file_name = "test.json";
+
+    seneca::example::file_creator creator(file_path);
+
+    nlohmann::json json_data = {
+        {"name", "Dave"},
+        {"language", "C++"}
+    };
+
+    creator.create_file(json_data, file_name);
+
+    auto read_data = creator.read_file(file_name);
+
+    EXPECT_EQ(read_data["name"], "Dave");
+    EXPECT_EQ(read_data["language"], "C++");
+
+    std::filesystem::remove_all(test_dir);
 }
